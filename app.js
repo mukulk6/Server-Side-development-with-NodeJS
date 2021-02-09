8cb492a892424da41d8338965e83f1fd9fdf984d
mukulk6 <mynameismukul@gmail.com>
2021-02-09 20:56:04 +0530

    Week 3 module 2 - Cookies

--- app.js
+++ app.js
@@ -32,10 +32,52 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('1234567890'));
 
 function auth(req,res,next){
-  console.log(req.headers);
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user)
+  {
+    var authHeader = req.headers.authorization;
+    if(!authHeader)
+  {
+    var err=new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status=401;
+    return next(err);
+  }
+  var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username == 'admin' && password=='password')
+  {
+    res.cookie('user','admin',{signed:true})
+    next();
+  }
+  else
+  {
+    var err=new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status=401;
+    return next(err);
+  }
+  }
+  else
+  {
+    if(req.signedCookies.user=='admin')
+    {
+      next();
+    }
+    else
+    {
+      var err=new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      return next(err);
+    }
+  }
   var authHeader = req.headers.authorization;
   if(!authHeader)
   {
@@ -45,7 +87,7 @@ function auth(req,res,next){
     return next(err);
   }
 
-  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+  var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
 
   var username = auth[0];
   var password = auth[1];
